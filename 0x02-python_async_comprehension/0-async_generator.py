04e6690521b1f1da9d1cf712dca6508daa5a8f2d
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-12 22:07:36 +0300

    Random.random() * 10 instead of random.randint()

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,12 +5,13 @@ A coroutine that takes no arguments
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """
     Yields 10 random numbers between 0 and 10.
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.random() * 10
