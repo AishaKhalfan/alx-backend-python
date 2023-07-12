1ae3c22bc5e704fc343b252f0cc5f3e563e80b11
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-12 23:19:25 +0300

    Pycodestyle

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,14 +4,12 @@ A coroutine that takes no arguments
 imported from async_generator
 """
 
-import asyncio
-import random
-from typing import List
+from typing import Generator
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float:
+async def async_comprehension() -> Generator[float, None, None]:
     """
     Yields 10 random numbers.
     """
